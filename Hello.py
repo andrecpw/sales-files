4bc5c2adedf56f5ed294dcafbef52f4f2f511b6c
andrecpw <andre_cpw@hotmail.com>
2023-11-06 19:49:28 +0100

    form_v1

--- Hello.py
+++ Hello.py
@@ -1,51 +1,95 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# Function to process the form data (this is where you would add your PDF filling logic)
+def process_form_data(form_data):
+    # You can process and save the data, or generate a PDF using a library like PyPDF2 or reportlab
+    st.write("Form Submitted. Here's the data:")
+    st.json(form_data)  # For demonstration, just display the data as JSON in the app
 
+def main():
+    # Set page config
+    st.set_page_config(page_title="Ficha de Vendas", page_icon="📝")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    st.title('Ficha de Vendas')
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Use a form for better user experience (submits all at once)
+    with st.form(key='sales_form'):
+        st.subheader('Cliente:')
+        name = st.text_input('Nome:')
+        cpf = st.text_input('CPF:')
+        rg = st.text_input('RG:')
+        birth_date = st.date_input('Data de Nascimento:')
+        phone = st.text_input('Telefone:')
+        mobile = st.text_input('Celular:')
+        street = st.text_input('Rua:')
+        street_no = st.text_input('No:')
+        district = st.text_input('Bairro:')
+        city = st.text_input('Cidade:')
+        cep = st.text_input('CEP:')
+        email = st.text_input('Email:')
 
-    st.sidebar.success("Select a demo above.")
+        st.subheader('Negociação:')
+        vehicle = st.text_input('Veículo:')
+        model = st.text_input('Modelo:')
+        color = st.text_input('Cor:')
+        year_model = st.text_input('Ano/Modelo:')
+        chassis = st.text_input('Chassi:')
+        price = st.text_input('Preço:')
+        plate = st.text_input('Placa:')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        st.subheader('Veículo Usado:')
+        used_plate = st.text_input('Placa:')
+        renavam = st.text_input('RENAVAM:')
+        chassis_used = st.text_input('Chassi:')
+        km = st.number_input('KM:', step=1)
+        payment_form = st.text_area('Forma de Pagamento:')
 
+        st.subheader('Observações:')
+        observations = st.text_area('Observações:')
+        authorization = st.checkbox('Autorizo a retirar o veículo acima descrito')
+
+        st.subheader('Serviços:')
+        emplacamento = st.checkbox('Emplacamento/Transferência')
+        ipva = st.checkbox('IPVA')
+        plate_choice = st.checkbox('Escolha de Placa')
+
+        # Form submit button
+        submit_button = st.form_submit_button(label='Submit')
+
+    # Process the form data when the button is pressed
+    if submit_button:
+        form_data = {
+            'name': name,
+            'cpf': cpf,
+            'rg': rg,
+            'birth_date': str(birth_date),
+            'phone': phone,
+            'mobile': mobile,
+            'street': street,
+            'street_no': street_no,
+            'district': district,
+            'city': city,
+            'cep': cep,
+            'email': email,
+            'vehicle': vehicle,
+            'model': model,
+            'color': color,
+            'year_model': year_model,
+            'chassis': chassis,
+            'price': price,
+            'plate': plate,
+            'used_plate': used_plate,
+            'renavam': renavam,
+            'chassis_used': chassis_used,
+            'km': km,
+            'payment_form': payment_form,
+            'observations': observations,
+            'authorization': authorization,
+            'emplacamento': emplacamento,
+            'ipva': ipva,
+            'plate_choice': plate_choice
+        }
+        process_form_data(form_data)
 
 if __name__ == "__main__":
-    run()
+    main()
