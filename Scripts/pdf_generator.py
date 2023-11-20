120d309df41ecca75672b1b4994ae64a1fc032f5
andrecpw <andre_cpw@hotmail.com>
2023-11-20 13:22:23 +0100

    fill_pdf_update

--- Scripts/pdf_generator.py
+++ Scripts/pdf_generator.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfReader, PdfWriter, PdfName
 
 def fill_pdf(input_pdf_path, output_pdf_path, form_data):
     reader = PdfReader(input_pdf_path)
@@ -6,27 +6,29 @@ def fill_pdf(input_pdf_path, output_pdf_path, form_data):
 
     # Iterate through each page in the PDF
     for page in reader.pages:
-        # Get the fields in the current page
-        fields = page.get('/Annots')
+        # Get the annotations (fields) in the current page
+        annotations = page.get("/Annots")
 
-        if fields is not None:
-            for field in fields:
-                # Resolve indirect object references
-                field = field.get_object()
+        # Check if annotations exist
+        if annotations:
+            # Resolve each annotation if it is an indirect object
+            for annotation in annotations:
+                if isinstance(annotation, IndirectObject):
+                    annotation = annotation.get_object()
                 
                 # Check if it is a form field (widget annotation)
-                if field.get('/Subtype') == '/Widget' and field.get('/T'):
-                    field_name = field['/T'].strip('()')
-                    
+                if annotation.get("/Subtype") == "/Widget" and annotation.get("/T"):
+                    field_name = annotation["/T"].strip("()")
+
                     # Update the field value if the field name is in form_data
                     if field_name in form_data and form_data[field_name] is not None:
-                        field.update({
-                            '/V': form_data[field_name]
+                        annotation.update({
+                            PdfName("/V"): form_data[field_name]
                         })
-            
-            # Update the page with modified fields
-            writer.add_page(page)
+
+        # Add the updated page to the writer
+        writer.add_page(page)
 
     # Write the output PDF file
-    with open(output_pdf_path, 'wb') as output_stream:
+    with open(output_pdf_path, "wb") as output_stream:
         writer.write(output_stream)
