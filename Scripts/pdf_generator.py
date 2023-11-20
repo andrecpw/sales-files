4dc8c5ea28586e569f65bf72f6023f94cb22bd3f
andrecpw <andre_cpw@hotmail.com>
2023-11-20 13:03:27 +0100

    PyPDF2

--- Scripts/pdf_generator.py
+++ Scripts/pdf_generator.py
@@ -1,16 +1,32 @@
-import pdfrw
+from PyPDF2 import PdfReader, PdfWriter
 
 def fill_pdf(input_pdf_path, output_pdf_path, form_data):
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    annotations = template_pdf.pages[0]['/Annots']
+    reader = PdfReader(input_pdf_path)
+    writer = PdfWriter()
 
-    for annotation in annotations:
-        if annotation['/Subtype'] == '/Widget':
-            if annotation['/T']:
-                key = annotation['/T'][1:-1]  # Remove parentheses
-                if key in form_data:
-                    annotation.update(
-                        pdfrw.PdfDict(V='{}'.format(form_data[key]))
-                    )
+    # Iterate through each page in the PDF
+    for page in reader.pages:
+        # Get the fields in the current page
+        fields = page.get('/Annots')
 
-    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+        if fields is not None:
+            for field in fields:
+                # Resolve indirect object references
+                field = field.get_object()
+                
+                # Check if it is a form field (widget annotation)
+                if field.get('/Subtype') == '/Widget' and field.get('/T'):
+                    field_name = field['/T'].strip('()')
+                    
+                    # Update the field value if the field name is in form_data
+                    if field_name in form_data and form_data[field_name] is not None:
+                        field.update({
+                            '/V': form_data[field_name]
+                        })
+            
+            # Update the page with modified fields
+            writer.add_page(page)
+
+    # Write the output PDF file
+    with open(output_pdf_path, 'wb') as output_stream:
+        writer.write(output_stream)
