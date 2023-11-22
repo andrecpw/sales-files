7adc4c8676ccb193caa4dfa6e5a49858499dc7f4
andrecpw <andre_cpw@hotmail.com>
2023-11-22 14:54:06 +0100

    lojas_processing

--- Hello.py
+++ Hello.py
@@ -36,6 +36,40 @@ def process_form_data(form_data):
     for i in range(1, 7):
         form_data[f"NEGOCIACAO_{i}"] = negociacao_parts[i - 1] if i <= len(negociacao_parts) else ""
 
+    # Process LOJA if Used_car
+    if form_data.get("PLACA", "") != "" or form_data.get("USADO_VEICULO", "") != "":
+        if form_data.get("LOJA") == "Promenac Matriz":
+            form_data["LOJA"] = "REVENDEDORES PROMENAC LTDA"
+            form_data["LOJA_CNPJ"] = "84.290.907/0001-22"
+            form_data["LOJA_ENDERECO"] = "RUA EXPEDICIONÁRIO ALEIXO AMABA, 60 - BARRA DO RIO"
+            form_data["LOJA_CIDADE"] = "ITAJAÍ / SC"
+        elif form_data.get("LOJA") == "Camvel":
+            form_data["LOJA"] = "CAMBORIÚ COMÉRCIO DE VEÍCULOS LTDA"
+            form_data["LOJA_CNPJ"] = "83.115.493/0001-32"
+            form_data["LOJA_ENDERECO"] = "AV. MARGINAL LESTE , N° 3500 - DOS ESTADOS"
+            form_data["LOJA_CIDADE"] = "BALNEÁRIO CAMBORIÚ / SC"
+        elif form_data.get("LOJA") == "Caninana":
+            form_data["LOJA"] = "REVENDEDORES PROMENAC LTDA"
+            form_data["LOJA_CNPJ"] = "84.290.907/0003-94"
+            form_data["LOJA_ENDERECO"] = "IRINEU BORNHAUSEN, N° 301 - SÃO JOÃO"
+            form_data["LOJA_CIDADE"] = "ITAJAÍ / SC"
+        elif form_data.get("LOJA") == "Brava":
+            form_data["LOJA"] = "REVENDEDORES PROMENAC LTDA"
+            form_data["LOJA_CNPJ"] = "84.290.907/0002-03"
+            form_data["LOJA_ENDERECO"] = "RODOVIA OSVALDO REIS, N° 2455 - FAZENDA"
+            form_data["LOJA_CIDADE"] = "ITAJAÍ / SC"
+        elif form_data.get("LOJA") == "Porto Belo":
+            form_data["LOJA"] = "CAMBORIÚ COMÉRCIO DE VEÍCULOS LTDA"
+            form_data["LOJA_CNPJ"] = "83.115.493/0003-02"
+            form_data["LOJA_ENDERECO"] = "AV. GOVERNADOR CELSO RAMOS, N° 1393 - PEREQUÊ"
+            form_data["LOJA_CIDADE"] = "PORTO BELO / SC"
+        elif form_data.get("LOJA") == "Penha":
+            form_data["LOJA"] = "REVENDEDORES PROMENAC LTDA"
+            form_data["LOJA_CNPJ"] = "84.290.907/0004-75"
+            form_data["LOJA_ENDERECO"] = "AV PREFEITO EUGENIO KRAUSE, N° 283 - CENTRO"
+            form_data["LOJA_CIDADE"] = "PENHA / SC"
+
+
     # Convert all string values to uppercase
     form_data = {key: (value.upper() if value is not None else None) 
              for key, value in form_data.items() if isinstance(value, str)}
@@ -97,7 +131,7 @@ def main():
         MARCA = st.text_input('Marca:')
         MODELO = st.text_input('Modelo:')
         OPCIONAIS = st.text_input('Opcionais:')
-        COMBUSTIVEL = st.text_input('Combustível:')
+        COMBUSTIVEL = st.radio('Combustível:', ["Total Flex", "Gasolina", "Diesel", "GNV", "Elétrico"])
         COR = st.text_input('Cor:')
         PY = st.text_input('Ano de Produção:')
         MY = st.text_input('Ano do Modelo:')
@@ -117,6 +151,7 @@ def main():
         USADO_MY = st.text_input('Ano Modelo:')
         USADO_KM = st.text_input('KM do Veículo Usado:')
         USADO_QUITACAO = st.text_input('Quitação:')
+        LOJA = st.radio("Loja compradora:", ["Promenac Matriz", "Camvel", "Caninana", "Brava", "Porto Belo", "Penha"])
 
         st.subheader('Forma de Pagamento:')
         NF = st.text_input('Valor Nota Fiscal:')
@@ -185,6 +220,7 @@ def main():
             'USADO_PY_MY': USADO_PY + "/" + USADO_MY,
             'USADO_KM': USADO_KM,
             'USADO_QUITACAO': USADO_QUITACAO,
+            'LOJA': LOJA,
 
             'NF': NF,
             'FINANCIAMENTO': FINANCIAMENTO,
@@ -211,7 +247,7 @@ def main():
         pdf_paths.append(create_pdf_and_return_path("Templates/NOVA Ficha de Vendas V4.pdf", form_data, "FV", font_size=9))
 
         # Fill Procuração de Comprador based on CPF length
-        if form_data.get("PLACA", "") != "":
+        if form_data.get("PLACA", "") != "" or form_data.get("USADO_VEICULO", "") != "":
             if form_data.get("PESSOA_FISICA", "") == "S":
                 pdf_paths.append(create_pdf_and_return_path("Templates/PROCURAÇÃO DE COMPRADOR PF.pdf", form_data, "Proc"))
             elif form_data.get("PESSOA_FISICA", "") == "N":
